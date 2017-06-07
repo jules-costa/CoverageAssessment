cc28ee6a3c51061f200ad1e167faf15f2cfecb3a
Julianne Costa <julesleighcosta@gmail.com>
2017-06-07 16:34:48 -0700

    code up session reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,14 @@
+import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
+
+const sessionReducer = (state = null, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      const currentUser = action.currentUser;
+      return currentUser;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
