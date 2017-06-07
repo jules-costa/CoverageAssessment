2041fd5680a23f519a75de7c13af8dbfd902023c
Julianne Costa <julesleighcosta@gmail.com>
2017-06-07 15:57:08 -0700

    session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,43 @@
+import * as SessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const clearErrors = () => ({
+  type: CLEAR_ERRORS
+});
+
+export const signup = user => dispatch => (
+  SessionAPIUtil.signup(user).then(currentUser => {
+    dispatch(receiveCurrentUser(currentUser));
+    dispatch(clearErrors());
+  }, err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const login = user => dispatch => (
+  SessionAPIUtil.login(user).then(currentUser => {
+    dispatch(receiveCurrentUser(currentUser));
+    dispatch(clearErrors());
+  }, err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
+
+export const logout = () => dispatch => (
+  SessionAPIUtil.logout().then(user => (
+    dispatch(receiveCurrentUser(null))
+  ))
+)
