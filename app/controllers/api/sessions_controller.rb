d7dd83cbb7c97dd07e3b76cb08d41d2881999002
Julianne Costa <julesleighcosta@gmail.com>
2017-06-06 19:55:12 -0700

    code up users and sessions controllers without view rendering

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,16 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    if @user
+      login(@user)
+      # render "api/stories"
+    else
+      render json: @user.errors.full_messages, status: 401
+    end
   end
 
   def destroy
+    logout
+    # render "api/sessions/"
   end
 end
