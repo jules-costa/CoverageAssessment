c3509f2f2dc529563d86d407d9f30b1a9dfbb708
Julianne Costa <julesleighcosta@gmail.com>
2017-06-07 17:45:34 -0700

    latch into Root component in entry file

--- frontend/coverage_assessment.jsx
+++ frontend/coverage_assessment.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
   window.store = configureStore();
-  ReactDOM.render(<h1>to be replaced</h1>, root);
+  ReactDOM.render(<Root store={ store } />, root);
 });
