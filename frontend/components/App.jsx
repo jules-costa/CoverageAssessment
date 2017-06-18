1bafcbadb3374bf2f60f1649aaaaffbebe3962d3
Julianne Costa <julesleighcosta@gmail.com>
2017-06-17 20:54:41 -0700

    add route to App for posts index container

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SessionContainer from './auth/session_container';
 import GreetingContainer from './greeting';
+import PostIndexContainer from './posts/post_index_container';
 import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
   <div>
     <Route exact path="/" component={GreetingContainer} />
     <Route path="/auth" component={SessionContainer} />
+    <Route path="/posts" component={PostIndexContainer} />
   </div>
 );
 
