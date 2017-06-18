403bea73fc3a5371ec5e197e7faa29ea325b6212
Julianne Costa <julesleighcosta@gmail.com>
2017-06-17 20:33:05 -0700

    generate posts controller and organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-
   root to: "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
+    resources :posts, only: [:create, :update, :index, :show, :destroy]
     resource :session, only: [:create, :destroy]
   end
 
