9928dadb2ea676bb08d002a2b0b5a816f25ea435
Julianne Costa <julesleighcosta@gmail.com>
2017-06-17 21:00:07 -0700

    post index

---
+++ frontend/components/posts/post_index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import PostIndexItem from './post_index_item';
+
+class PostIndex extends React.Component {
+  componentDidMount() {
+    this.props.getPosts();
+  }
+
+  render() {
+    return(
+      <div className="feed-wrapper">
+        <section className="feed">
+          {this.props.posts.map((post, i) => <PostIndexItem key={post.id} post={post} />)}
+        </section>
+      </div>
+    );
+  }
+}
+
+
+export default PostIndex;
