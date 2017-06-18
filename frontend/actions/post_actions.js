8fc6af537e91680de40f781e43787918522ab2c4
Julianne Costa <julesleighcosta@gmail.com>
2017-06-17 20:47:28 -0700

    configure posts api util and actions

---
+++ frontend/actions/post_actions.js
@@ -0,0 +1,18 @@
+import * as PostAPIUtil from '../util/post_api_util';
+import { receiveErrors, clearErrors, RECEIVE_ERRORS, CLEAR_ERRORS } from './session_api_util';
+
+export const RECEIVE_ALL_POSTS = "RECEIVE_ALL_POSTS";
+
+export const receiveAllPosts = posts => ({
+  type: RECEIVE_ALL_POSTS,
+  posts
+});
+
+export const getPosts = () => dispatch => (
+  PostAPIUtil.fetchAllPosts().then(posts => {
+    dispatch(receiveAllPosts(posts));
+    dispatch(clearErrors());
+  }, err => (
+    dispatch(receiveErrors(err.responseJSON))
+  ))
+);
