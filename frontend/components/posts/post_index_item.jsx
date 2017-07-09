12c96b2f613342e0ed43590dc717ead82593e042
Julianne Costa <julesleighcosta@gmail.com>
2017-07-08 17:13:00 -0700

    reconfigure seeds using html

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ReactHtmlParser from 'react-html-parser';
 
 const PostIndexItem = ({ post }) => (
     <section className="feed-item-wrapper">
@@ -7,7 +8,7 @@ const PostIndexItem = ({ post }) => (
         <Link to={`/posts/${post.id}`}>
           <h1 className="post-title">{post.title}</h1>
         </Link>
-        <h4 className="post-body">{post.truncatedBody}</h4>
+        <h4 className="post-body">{ ReactHtmlParser(post.truncatedBody) }</h4>
       </section>
     </section>
 );
