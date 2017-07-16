6602f24e0540503a4c5b836cf3e6cd18db7185b2
Julianne Costa <julesleighcosta@gmail.com>
2017-07-15 18:19:29 -0700

    rendering blank fields on form

--- frontend/components/posts/post_form.jsx
+++ frontend/components/posts/post_form.jsx
@@ -3,15 +3,31 @@ import React from 'react';
 class PostForm extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   hey: hey
-    // }
+    this.state = {
+      title: "",
+      body: "",
+      image_url: "",
+      category_id: null
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.update = this.update.bind(this);
+  }
+
+  update(field) {
+    this.setState([field]: )
+  }
+
+  handleSubmit(e) {
+    e.preventDefault;
 
   }
 
   render () {
     return (
-      <h1>In the post form</h1>
+      <form onSubmit={ this.handleSubmit }>
+        <input type="text" name="title" value={ this.state.title } onChange={ this.update("title") }/>
+        <textarea type="text" name="body" value={ this.state.body } onChange={ this.update("body") }></textarea>
+      </form>
     )
   }
 }
