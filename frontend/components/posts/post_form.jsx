b7077a204b2e5f2763fb5319c0f2e52b64d4baa0
Julianne Costa <julesleighcosta@gmail.com>
2017-07-15 17:44:00 -0700

    begin post form container and presentation

---
+++ frontend/components/posts/post_form.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class PostForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+
+  }
+
+  render () {
+    return (
+      <h1>In the post form</h1>
+    )
+  }
+}
+
+export default PostForm;
---
+++ frontend/components/posts/post_form_container.js
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import PostForm from './post_form';
+import { createPost, updatePost } from '../actions/post_actions';
+
+const mapStateToProps = state => ({
+  state: state
+});
+
+const mapDispatchToProps = dispatch => ({
+  createPost: (post) => dispatch(createPost(post)),
+  updatePost: (post) => dispatch(updatePost(post))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PostForm)
