dd7419f0b43c92a7a92b1d9e0747341722ee1069
Julianne Costa <julesleighcosta@gmail.com>
2017-07-19 21:16:21 -0700

    fix action view template rendering

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create]
+    resources :users, only: [:create, :show]
     resources :posts, only: [:create, :update, :index, :show, :destroy]
     resource :session, only: [:create, :destroy]
   end
--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -4,6 +4,7 @@ const errorsReducer = (state = [], action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_ERRORS:
+    console.log("here");
       return action.errors;
     case CLEAR_ERRORS:
       return [];
