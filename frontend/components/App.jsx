e7e9135a94bdcf35e935364059770366a5632080
Julianne Costa <julesleighcosta@gmail.com>
2017-07-19 18:43:33 -0700

    reconfigure paths

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -8,11 +8,10 @@ import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
   <div>
-    <Route exact path="/about" component={GreetingContainer} />
     <Route exact path="/" component={PostIndexContainer} />
-    <Route exact path="/" component={SessionContainer} />
-    {/* <Route exact path="/signup" component={SessionContainer} /> */}
-    {/* <Route exact path="/posts" component={PostIndexContainer} /> */}
+    <Route exact path="/about" component={GreetingContainer} />
+    <Route exact path="/login" component={SessionContainer} />
+    <Route exact path="/signup" component={SessionContainer} />
     <Route exact path="/posts/:id" component={PostDetailContainer} />
     <Route exact path="/write" component={PostFormContainer} />
   </div>
