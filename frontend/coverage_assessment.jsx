6d2d10fa115d0247fae542ce684bc70acb76e6f6
Julianne Costa <julesleighcosta@gmail.com>
2017-07-20 09:26:43 -0700

    receiving currentUser

--- frontend/coverage_assessment.jsx
+++ frontend/coverage_assessment.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Modal from 'react-modal';
 import Root from './components/root';
 import configureStore from './store/store';
-
-import { createPost } from './actions/post_actions';
 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
-  Modal.setAppElement(document.body);
+  window.getState = store.getState;
   ReactDOM.render(<Root store={ store } />, root);
 });
