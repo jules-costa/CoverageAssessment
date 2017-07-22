0d59f2526c1272a61fc891cdc66d835e72d5a883
Julianne Costa <julesleighcosta@gmail.com>
2017-07-22 15:20:53 -0700

    jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery-ujs
-//= jquery
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
